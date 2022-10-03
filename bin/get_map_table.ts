4241311c6777cc3135fcb5b5cbdfdfee65136db2
nanikit <nanikit4@gmail.com>
2022-10-04 03:43:46 +0900

    feat: amend oneshot

--- bin/get_map_table.ts
+++ bin/get_map_table.ts
@@ -1,38 +1,21 @@
 import { getDetailFromId } from "../src/beatsaver.ts";
 
-type ScoreboardLevel = {
-  name: string;
-  diff: string;
-  code: string;
-  hash: string;
-  note: number;
-};
-
-async function getMapIds() {
-  for (let i = 0; i < 4; i++) {
-    const response = await fetch(
-      `https://raw.githubusercontent.com/DetegiCE/KBSL3-Qualifiers-Scoreboard/main/data/songs/songs${
-        i + 1
-      }.json`,
-    );
-    const json = (await response.json()) as { songs: ScoreboardLevel[] };
-    console.log(`Processing index songs${i + 1}.json.`);
-    for (const level of json.songs) {
-      const detail = await getDetailFromId(level.code);
-      console.log([
-        level.name,
-        detail.metadata.songAuthorName,
-        detail.metadata.levelAuthorName,
-        level.diff,
-        `[${level.code}](https://beatsaver.com/maps/${level.code})`,
-        `[미리보기](https://skystudioapps.com/bs-viewer/?id=${level.code})`,
-      ].join(" | "));
-    }
+async function getMapIds(ids: string[]) {
+  for (const id of ids) {
+    const detail = await getDetailFromId(id);
+    console.log([
+      detail.name,
+      detail.metadata.songAuthorName,
+      detail.metadata.levelAuthorName,
+      "Expert+",
+      `[${id}](https://beatsaver.com/maps/${id})`,
+      `[미리보기](https://skystudioapps.com/bs-viewer/?id=${id})`,
+    ].join(" | "));
   }
 }
 
 async function main() {
-  await getMapIds();
+  await getMapIds(Deno.args);
 }
 
 main();
